a22b04927d72e0e5625fe3516dce13dab482d050
Vadym <vad.pechenoga@gmail.com>
2015-04-25 15:37:22 +0300

    Create new car test

--- src/test/java/lab1_tests/Tests.java
+++ src/test/java/lab1_tests/Tests.java
@@ -1,21 +1,40 @@
 package lab1_tests;
 
 import lab1.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Tests {
+    private static MysqlCarDAO dao;
+    private  static SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+
+    @BeforeClass
+    public static void init() {
+        dao = new MysqlCarDAO();
+    }
+
     @Test
     public void getConnection() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        MysqlCarDAO dao = new MysqlCarDAO();
         Method getConnection = MysqlCarDAO.class.getDeclaredMethod("getConnection", null);
         getConnection.setAccessible(true);
         Connection connection = (Connection) getConnection.invoke(dao, null);
         assertNotNull(connection);
+    }
+
+    @Test
+    public void createNewCar() throws ParseException {
+        int sizeBefore = dao.getAll().size();
+        Car car = new Car("Honda", "Accord", new java.sql.Date(sdf.parse("2009").getTime()), 120, 230);
+        dao.create(car);
+        int sizeAfter = dao.getAll().size();
+        assertEquals(sizeBefore+1, sizeAfter);
     }
 
 }
